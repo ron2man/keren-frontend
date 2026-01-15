23a6fd3f2b8a02a46ac2c68f92d3bb6306aeacdc
vc-ron <ron@vcita.com>
2026-01-15 09:40:05 +0200

    Add route guard to prevent navigation to the same path

--- src/router/index.js
+++ src/router/index.js
@@ -37,4 +37,12 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.path === from.path) {
+    next(false)
+  } else {
+    next()
+  }
+})
+
 export default router
